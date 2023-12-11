7a8fdcefe9ed67995626866c4252cee06c5a6fe3
osquaaa <136095047+osquaaa@users.noreply.github.com>
2023-12-11 18:40:41 +0300

    7

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ function openNav() {
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginRight = "0";
+    document.getElementById("mySidenav").style.width = "0px";
+    document.getElementById("main").style.marginRight = "0px";
 }
