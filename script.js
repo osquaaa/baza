a33edc2c3fa3347669412dbd449e1cb5d591fbcd
osquaaa <osquaa@gmail.com>
2024-06-21 12:40:24 +0300

    сделал бургер меню

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginRight = "250px";
+    document.getElementById("mySidenav").style.width = "100%";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+/* Close/hide the sidenav */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0px";
-    document.getElementById("main").style.marginRight = "0px";
+    document.getElementById("mySidenav").style.width = "0";
 }
