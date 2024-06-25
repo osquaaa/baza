aa82a0c19e4e2057df004de20c525b249d7f6607
osquaaa <osquaa@gmail.com>
2024-06-25 14:11:11 +0300

    сделал кнопки обратного звонка

--- script.js
+++ script.js
@@ -12,20 +12,3 @@ function closeNav() {
 
 
 
-const modal = document.querySelector('#modal');
-const btn = document.querySelector('#openModal');
-const close = document.querySelector('.close');
-
-btn.onclick = function () {
-  modal.style.display = 'block';
-};
-
-close.onclick = function () {
-  modal.style.display = 'none';
-};
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = 'none';
-  }
-};
