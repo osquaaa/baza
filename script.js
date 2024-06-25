12eb758ebb02e0df3ac9f29bc389608895ad91c0
osquaaa <osquaa@gmail.com>
2024-06-25 13:22:43 +0300

    сырое модальное окно и соцсети в футере

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+
+
+
+
+
+const modal = document.querySelector('#modal');
+const btn = document.querySelector('#openModal');
+const close = document.querySelector('.close');
+
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+close.onclick = function () {
+  modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
